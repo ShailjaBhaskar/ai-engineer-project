072a23ab279d57f7d99e6ff5bd4565b4c2569b93
Shailja Bhaskar <shailjabhaskar01@gmail.com>
2026-05-03 20:34:13 +0530

    Day 5: Implemented embeddings and similarity comparison

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,16 +1,37 @@
+import json
 from openai import OpenAI
 from app.core.config import OPENAI_API_KEY
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-def get_ai_response(user_input: str) -> str:
+
+def get_ai_response(user_input: str) -> dict:
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are an AI tutor. Answer clearly, concisely, and in bullet points. Don't exceed more than 3 points."},
-            {"role": "user", "content": user_input}
+            {
+                "role": "system",
+                "content": """
+You are a JSON generator.
+
+Rules:
+1. Always return valid JSON
+2. Do NOT include any extra text
+3. Format:
+{
+  "answer": "..."
+}
+""",
+            },
+            {"role": "user", "content": user_input},
         ],
-        temperature=0.7
+        temperature=0.3,
     )
 
-    return response.choices[0].message.content
+    content = response.choices[0].message.content
+
+    # 🔥 Step 2: Parse JSON safely
+    try:
+        return json.loads(content)
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON response", "raw": content}
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from app.services.ai_service import get_ai_response
+# from app.services.ai_service import get_ai_response
+
+# def main():
+#     while True:
+#         user_input = input("You: ")
+        
+#         if user_input.lower() == "exit":
+#             break
+        
+#         response = get_ai_response(user_input)
+#         print("AI:", response)
+
+# if __name__ == "__main__":
+#     main()
+
+from app.services.embedding_service import get_embedding
+import numpy as np
+
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))    
 
 def main():
-    while True:
-        user_input = input("You: ")
-        
-        if user_input.lower() == "exit":
-            break
-        
-        response = get_ai_response(user_input)
-        print("AI:", response)
+    emb1 = get_embedding("AI")
+    emb2 = get_embedding("Artificial Intelligence")
+
+    print("AI:", emb1[:5])
+    print("Artificial Intelligence:", emb2[:5])
+    vec1 = get_embedding("AI")
+    vec2 = get_embedding("Artificial Intelligence")
+    vec3 = get_embedding("Pizza")
+
+    print("AI vs AI:", cosine_similarity(vec1, vec1))
+    print("AI vs Artificial Intelligence:", cosine_similarity(vec1, vec2))
+    print("AI vs Pizza:", cosine_similarity(vec1, vec3))
+
 
 if __name__ == "__main__":
     main()
